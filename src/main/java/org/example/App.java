e616aea83e56ccee01f5fadf9fdb07a578ae49b0
Khushimc <khushivarshini1030@gmail.com>
2025-12-26 23:08:25 +0530

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,5 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven + Jenkins!");
         System.out.println("hello world");
+        System.out.println("Hello from Maven + Jenkins!");
+
+        
     }
 }
